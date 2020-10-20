69caa915ddb160bb19c1b492b512877b00fab25b
ttack-hub0706 <tkm_fjhr0706@icloud.com>
2020-10-21 00:19:57 +0900

    first commit

---
+++ src/components/SideNavi.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="left-padding">
+        <img src="../assets/shareimg/logo.png" alt="ろご" class="logo">
+        <div class="flex icon-field">
+          <img src="../assets/shareimg/home.png" alt="ホーム" class="icon">
+          <p>ホーム</p>
+        </div>
+        <div
+        class="flex icon-field">
+          <img src="../assets/shareimg/profile.png" alt="" class="icon">
+          <p>プロフィール</p>
+        </div>
+        <div class="flex icon-field">
+          <img src="../assets/shareimg/logout.png" alt="" class="icon">
+          <p>ログアウト</p>
+        </div>
+      </div>
+      <hr />
+      <ShareMessage />
+  </div>
+</template>
+
+<script>
+import ShareMessage from "../components/ShareMessage";
+export default {
+  components:{
+    ShareMessage
+  }
+}
+</script>
+
+<style scoped>
+.left-padding {
+ margin: 20px;
+}
+.logo {
+ width: 100px;
+}
+.icon-field {
+ margin-top: 15px;
+ cursor: pointer;
+}
+.icon-field p {
+ font-size: 16px;
+ padding-left: 15px;
+}
+.flex{
+  display: flex;
+}
+.icon{
+  width: 25px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import Profile from '../views/Profile.vue';
+import Detail from '../views/Detail.vue';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/profile',
+    name: "profile",
+    component: Profile
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    component: Detail
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router;
